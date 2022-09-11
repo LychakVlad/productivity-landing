bb9f0628ffbb2f490e342ccc1bd8b3f76d450aa8
Vlad <lycakvladislav@gmail.com>
2022-09-11 14:11:05 +0200

    First Commit

---
+++ gulp/config/ftp.js
@@ -0,0 +1,6 @@
+export let configFTP = {
+    host: "", // Адрес FTP сервера
+    user: "Vlad", // Имя пользователя 
+    password: "2329", // Пароль 
+    parallel: 5 // Кол-во одновременных потоков
+}
